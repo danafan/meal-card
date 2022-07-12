8c79eb6f247a0b802b48bbf19b286a5ad867a75b
范玉龙 <13067882143@163.com>
2022-07-12 13:17:00 +0800

    新版参卡

---
+++ components/menu-item/menu-item.js
@@ -0,0 +1,11 @@
+Component({
+  mixins: [],
+  data: {},
+  props: {
+    type:"",    //add_menu:‘用户点餐’
+  },
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
--- page/index/index.js
+++ page/index/index.js
@@ -1,11 +1,17 @@
 const resource = require('../../utils/api.js').API;
 Page({
   data: {
-
+    user_type: 1,    //1:用户；商家
   },
   onLoad() {
+    // this.setData({
+    //   user_type: 1
+    // })
+    dd.navigateTo({
+        url: '/page/user/choose_menu/choose_menu'
+      })
     //钉钉获取用户信息
-    this.getDingInfo();
+    // this.getDingInfo();
   },
   //钉钉获取用户信息
   getDingInfo() {
@@ -22,16 +28,64 @@ Page({
     }).then(res => {
       getApp().globalData.user_info = res.data;
       getApp().globalData.token = res.token;
-      if (res.user_type == 1) { //用户
-        dd.redirectTo({
-          url: '/page/user/index/index'
-        })
-      } else {          //商家
-        dd.redirectTo({
-          url: '/page/store/index/index'
-        })
-      }
+      this.setData({
+        user_type: res.user_type
+      })
     })
   },
+  //付款码或商家收款
+  fkmFn() {
+    if (this.data.user_type == '1') {
+      dd.navigateTo({
+        url: '/page/user/index/index'
+      })
+    } else {
+      dd.navigateTo({
+        url: '/page/store/index/index'
+      })
+    }
+  },
+  //跳转账单
+  goBill() {
+    if (this.data.user_type == '1') {
+      dd.navigateTo({
+        url: '/page/user/user_bill/user_bill'
+      })
+    } else {
+      dd.navigateTo({
+        url: '/page/store/store_bill/store_bill'
+      })
+    }
+  },
+  //点餐/上架菜品
+  dcFn() {
+    if (this.data.user_type == '1') {
+      dd.navigateTo({
+        url: '/page/user/choose_menu/choose_menu'
+      })
+    } else {
+      dd.navigateTo({
+        url: '/page/store/up_menu/up_menu'
+      })
+    }
+  },
+  //订单
+  orderFn() {
+    if (this.data.user_type == '1') {
+      dd.navigateTo({
+        url: '/page/user/user_order/user_order'
+      })
+    } else {
+      dd.navigateTo({
+        url: '/page/store/store_order/store_order'
+      })
+    }
+  },
+  //商家菜单管理
+  menuFn() {
+    dd.navigateTo({
+      url: '/page/store/menu_management/menu_management'
+    })
+  }
 
 })
